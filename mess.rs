abbf423ebd71e4ae6a0efa6f2ed5ecbfffe0aff8
nourabdulnasser <85139658+nourabdulnasser@users.noreply.github.com>
2022-05-01 19:45:04 +0200

    Update mess.rs

--- mess.rs
+++ mess.rs
@@ -1,108 +1,62 @@
-use std::env;
-use rand::Rng;
-use std::{thread, time};
-use std::io::{stdin,stdout,Write};
-//use std::process::Command;
 
 
-pub fn cards_memory(length: usize, time: usize){
+/* Addition to main.rs
+} else if mode == "-names" {
+       /* let amount_of_names = args[2].parse::<usize>().unwrap();
+        let amount_of_seconds_per_names = args[3].parse::<usize>().unwrap();
+        names::memory_names(amount_of_names,amount_of_seconds_per_names);
+        */
+        names::memory_names();
+    }
+*/
 
-    //use random_string::generate;
+// --------- names.rs
 
-    let ranks = vec!["A","K","Q","J","2","3","4","5","6","7","8","9","10"];
-    let suits = vec!["c","d","h","s"]; 
-    //let suits = vec!["♣","♦","♥","♠"];
+use super::api_call;
+use std::{thread};
+use terminal::{Clear, Action};
+// regex = "1.4.5"
+
+pub fn memory_names(){
+    let mut random_names: &str = "";
+    let mut names_vec: Vec<&str> = vec![];  // vector of clean random naames
+    let mut counter = 0;
+
+    // function for getting random instance through api and putting it in a vector
+    fn get_api_vec() -> Vec<String>{
+        let mut api_name_vec: Vec<&str> = vec![]; // vector of names extracted from api function
+        let api_name: String = api_call::api_get_request("https://random-data-api.com/api/name/random_name");
+        let api_delimit = regex::Regex::new(r",|:").unwrap(); // delimit characters for easier iteration
+        let api_delimit_vec: Vec<&str> = api_delimit.split(&api_name).collect(); // vector of all elements in api
     
-    // random suits and ranks
+        // trying to convert elements of api_delimit_vec into String
+        for element in api_delimit_vec{
+            let jk = element.to_string();
+            api_name_vec.push(jk);
+        } 
+        println!("{:?}",api_name_vec); // test
+        return api_name_vec;
+    }
 
-    let random_card = || {
+    // get_api_vec(); // test
 
-        let mut rng = rand::thread_rng();
+    let number_of_names: usize;
 
-        let rand_index_rank = rng.gen_range(0..ranks.len()-1);
-        let rand_index_suit = rng.gen_range(0..suits.len()-1);
-    
-        let rank = ranks[rand_index_rank];
-        let suit = suits[rand_index_suit];
-
-        //println!("{}", rank.to_owned()+suit);
-
-        return rank.to_owned()+suit;
-
-        // looks
-        // like
-        // this
-
-    };
-
-    // generating cards
-    
-    let mut cards_line: String = "".to_string();
-    let mut cards_vec = vec![];
-
-    for  card in 0..length{
-        //random_card();
-        let mut random_card_str = &random_card();
-        cards_line = cards_line + random_card_str;
-        cards_vec.push(random_card_str);
-        if card < length-1 {
-            cards_line = cards_line + ",  ";
+    // loop to get {number_of_names} random names
+    for number in 0..number_of_names
+    {
+        for element in &get_api_vec()
+        {
+          if get_api_vec()[counter] == "\"first_name\""
+            {
+                let random_name = get_api_vec()[counter+1];  // using let to bind
+                // random_name.replace("\"","");
+                names_vec.push(&random_name);
+            }
+            counter = counter + 1;
         }
 
-        // "looks,  like,  this" 
-
     }
-
-    println!("{:?}", cards_vec);
-
-   // println!("Memorise this:\n{{ {} }}\nYou have {} seconds.", cards_line, time);
-
-    // wait for time seconds
-
-    let mem_time_u64 = (length*time).try_into().unwrap(); 
-    
-    let mem_time = std::time::Duration::from_secs(mem_time_u64);
-
-    println!("Memorise this:\n{{ {} }}\nYou have {} seconds.", cards_line, length*time);
-
-    let now = time::Instant::now();
-
-    thread::sleep(mem_time);
-
-    print!("\x1B[2J\x1B[1;1H");  // not as clean as I'd like it to be but will do for now.
-
-    
-    
-    
-    //println!("what");
-    
-    
-    //std::process::Command::new("cls").status().unwrap();
-
-
-    // user prompt
-
-   /* let mut s=String::new();
-    print!("Type in what you memorised!");
-    let _=stdout().flush();
-    stdin().read_line(&mut s).expect("Did not enter a correct string");
-    if let Some('\n')=s.chars().next_back() {
-        s.pop();
-    }
-    if let Some('\r')=s.chars().next_back() {
-        s.pop();
-    }
-    println!("You typed: {}",s);*/
-
-
-
-
-    //println!("")
-    
-
-
-    
-    //let suit = "CDHS";
-    //println!("{}", generate(length, suit));
-
+    println!("{:?}", names_vec); // test
 }
+
